023f02e9e9ac32225dead33b19e8c873bf69c249
Aron <aronpjansen@gmail.com>
2023-06-16 15:27:41 +0200

    Format make

--- docs/make.jl
+++ docs/make.jl
@@ -1,25 +1,20 @@
 using GroupsOnGrids
 using Documenter
 
-DocMeta.setdocmeta!(GroupsOnGrids, :DocTestSetup, :(using GroupsOnGrids); recursive=true)
+DocMeta.setdocmeta!(GroupsOnGrids, :DocTestSetup, :(using GroupsOnGrids); recursive = true)
 
 makedocs(;
-    modules=[GroupsOnGrids],
-    authors="Aron <aronpjansen@gmail.com> and contributors",
-    repo="https://github.com/apjansen/GroupsOnGrids.jl/blob/{commit}{path}#{line}",
-    sitename="GroupsOnGrids.jl",
-    format=Documenter.HTML(;
-        prettyurls=get(ENV, "CI", "false") == "true",
-        canonical="https://apjansen.github.io/GroupsOnGrids.jl",
-        edit_link="main",
-        assets=String[],
+    modules = [GroupsOnGrids],
+    authors = "Aron <aronpjansen@gmail.com> and contributors",
+    repo = "https://github.com/apjansen/GroupsOnGrids.jl/blob/{commit}{path}#{line}",
+    sitename = "GroupsOnGrids.jl",
+    format = Documenter.HTML(;
+        prettyurls = get(ENV, "CI", "false") == "true",
+        canonical = "https://apjansen.github.io/GroupsOnGrids.jl",
+        edit_link = "main",
+        assets = String[],
     ),
-    pages=[
-        "Home" => "index.md",
-    ],
+    pages = ["Home" => "index.md"],
 )
 
-deploydocs(;
-    repo="github.com/apjansen/GroupsOnGrids.jl",
-    devbranch="main",
-)
+deploydocs(; repo = "github.com/apjansen/GroupsOnGrids.jl", devbranch = "main")
